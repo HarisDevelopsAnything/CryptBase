800f7c68a99b17b9c98dd0a4f473db3ad4ce5ffb
HarisDevelopsAnything <harisdevelops277@gmail.com>
2026-02-22 16:39:05 +0530

    Fixed build path

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import './App.css'
 import Ex1 from './pages/Ex1/Ex1'
 import Ex2 from './pages/Ex2/Ex2'
 import Ex3 from './pages/Ex3/Ex3'
+import Ex4 from './pages/Ex4/Ex4'
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Ex1/>
       <Ex2/>
       <Ex3/>
+      <Ex4/>
     </>
   )
 }
