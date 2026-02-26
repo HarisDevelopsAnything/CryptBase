9fa448c25639c2e80097e4776bfe3d5ea5f2ffd8
HarisDevelopsAnything <harisdevelops277@gmail.com>
2026-02-27 00:11:13 +0530

    Fixed port issue

--- backend/app.py
+++ backend/app.py
@@ -284,4 +284,6 @@ def decrypt_rsa():
 
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, port=port, host="0.0.0.0")
